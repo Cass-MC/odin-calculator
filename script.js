7fea1e3ef0d2d919b5828c81a708f78df246d735
Cassandra Cawley <cassandrabook09@gmail.com>
2022-10-12 12:20:45 -0700

    Don't allow multiple operations at once

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let histogramText = document.querySelector("#histogram");
+hookButtons();
+
 function add(x,y) {
     return x+y;
 }
@@ -29,31 +32,44 @@ function operate(o,x,y) {
             return divide(x,y);
             break;
     }
+    //add functionality for = here later
 }
 
 function display(button) {
     const buttonText = this.innerText;
-    let displayText = document.querySelector("#histogram");
-    displayText.innerText += buttonText;
+    let stopDuplicateOp = false;
+    if (histogramText.innerText.length > 0) { 
+        stopDuplicateOp = stopDuplicateOperations(buttonText, histogramText.innerText); 
+    }
+    if (stopDuplicateOp) { return; }
+    else { histogramText.innerText += buttonText; }
+
+}
+
+function stopDuplicateOperations(buttonText, histogramText) {
+    const lastButtonChar = String(buttonText)
+                            .charAt(buttonText.length - 1);
+    const lastDisplayChar = String(histogramText)
+                            .charAt(histogramText.length - 1);
+    const operationsReg = /[\+\-\*\/]/g;
+    if (lastButtonChar.match(operationsReg) && lastDisplayChar.match(operationsReg)) {
+        return true;
+    }
+    else { return false; }
 }
 
 function startOperation(button) {
-    const displayText = document.querySelector("#histogram");
-    
+    //hang on.
 }
 
 function hookButtons() {
     const allButtons = document.querySelectorAll("button");
     const operatorButtons = document.querySelectorAll(".operator");
-
     for (button of allButtons) {
         if (button.innerText === "Clear") { continue; }
         button.addEventListener("click", display);
     }
-
     for (button of operatorButtons) {
         button.addEventListener("click",startOperation);
     }
 }
-
-hookButtons();
