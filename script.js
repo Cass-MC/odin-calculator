a7a7fde8c42c07f5546696d555ff7f323063660f
Cassandra Cawley <cassandrabook09@gmail.com>
2022-10-18 15:26:27 -0700

    Reworked startOperation, tho starts to eval wrong

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let histogramText = document.querySelector("#histogram");
-let currentOp = document.querySelector("#currentOperation");
+let currentOpText = document.querySelector("#currentOperation");
+const operationsReg = /[\+\-\*\/]/g;
+let currentOp = 0;
 hookButtons();
 
 function add(x,y) {
@@ -39,6 +41,7 @@ function operate(o,x,y) {
 function display(button) {
     const buttonText = this.innerText;
     let stopDuplicateOp = false;
+
     if (histogramText.innerText.length > 0) { 
         stopDuplicateOp = stopDuplicateOperations(buttonText, histogramText.innerText); 
     }
@@ -47,9 +50,9 @@ function display(button) {
                                     .substring(0, histogramText.innerText.length - 1);
         histogramText.innerText = updateOperation + buttonText;
      }
-    else { histogramText.innerText += buttonText; }
-
-     currentOp.innerText = startOperation(button);
+    else { 
+        histogramText.innerText += buttonText;
+    }
 }
 
 function stopDuplicateOperations(buttonText, histogramText) {
@@ -57,7 +60,6 @@ function stopDuplicateOperations(buttonText, histogramText) {
                             .charAt(buttonText.length - 1);
     const lastDisplayChar = String(histogramText)
                             .charAt(histogramText.length - 1);
-    const operationsReg = /[\+\-\*\/]/g;
     if (lastButtonChar.match(operationsReg) && lastDisplayChar.match(operationsReg)) {
         return true;
     }
@@ -65,15 +67,27 @@ function stopDuplicateOperations(buttonText, histogramText) {
 }
 
 function startOperation(button) {
-    const operationsReg = /[\+\-\*\/]/g;
+    histoNumbers = histogramText.innerText.match(/[0-9]/g);
+    histoOperations = histogramText.innerText.match(operationsReg); 
 
-    //only works the first time..
-    if ((histogramText.innerText.match(operationsReg) || []).length == 2) {
-        let operation = histogramText.innerText.match(operationsReg)[0];
-        let nums = histogramText.innerText.match(/\d+/g);
-        console.log(nums);
-        return operate(operation, nums[0], nums[1]);
+    //Set up currentOp if there's enough data to do an operation
+    if (currentOp === 0 && histoOperations.length === 2) {
+        const op = histoOperations[histoOperations.length-2];
+        const num1 = Number(histoNumbers[histoNumbers.length-2]);
+        const num2 = Number(histoNumbers[histoNumbers.length-1]);
+        currentOp = operate(op, num1, num2);   
     }
+    //Once there's enough data to work with, work with it.
+    else if (histoOperations.length % 2 === 0 || histoOperations.length === 3) {
+        //last operation, last number
+        const op = histoOperations[histoOperations.length-2];
+        const num2 = Number(histoNumbers[histoNumbers.length-1]);
+        currentOp = operate(op, currentOp, num2);
+        console.log("heyo");
+    }
+    
+    currentOpText.innerText = currentOp;
+
 }
 
 function hookButtons() {
@@ -84,6 +98,6 @@ function hookButtons() {
         button.addEventListener("click", display);
     }
     for (button of operatorButtons) {
-        //button.addEventListener("click",startOperation);
+        button.addEventListener("click",startOperation);
     }
 }
